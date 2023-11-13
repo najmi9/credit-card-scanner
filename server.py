32f48d6c8048e7ee282fc878c21a0283503d08b2
najmi9 <imadnajmi9@gmail.com>
2023-11-13 01:25:13 +0100

    extract data form credit card: examle WebPay

--- server.py
+++ server.py
@@ -2,55 +2,24 @@ import os
 import uuid
 from flask import Flask, request, send_from_directory
 from flask_cors import CORS
-from PIL import Image
-import pytesseract
-import matplotlib.pyplot as plt
-import keras_ocr
 import easyocr
-
-def easyocr_read(file_path):
-    reader = easyocr.Reader(['en'])
-    result = reader.readtext(file_path)
-    text = ''
-    for i in range(len(result)):
-        text += result[i][1]
-    return text
-
-def keras_read(file_path):
-    image = keras_ocr.tools.read(file_path)
-    pipeline = keras_ocr.pipeline.Pipeline()
-    images = [image]
-    prediction_groups = pipeline.recognize(images)
-
-
-    recognized_text = [text for _, text in prediction_groups[0]]
-
-    """ fig, axs = plt.subplots(nrows=len(images), figsize=(20, 20))
-    for ax, image, predictions in zip(axs, images, prediction_groups):
-        keras_ocr.tools.drawAnnotations(image=image, predictions=predictions, ax=ax)
-
-    plt.show() """
-
-    return recognized_text
-
-def tesseract_read(file_path):
-    image = Image.open(file_path)
-    text = pytesseract.image_to_string(
-        image,
-        lang='eng'
-    )
-
-    return text
+import cv2
+import numpy as np
 
 app = Flask(__name__)
 CORS(app)
 
-# Define the path to the static files directory
-static_files_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static')
+static_files_dir = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'static'
+)
 
 @app.route('/', methods=['GET'])
 def index():
-    return send_from_directory(static_files_dir, 'index.html')
+    return send_from_directory(
+        static_files_dir,
+        'index.html'
+    )
 
 @app.route('/api/upload', methods=['POST'])
 def upload():
@@ -59,19 +28,66 @@ def upload():
     file_path = os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
         'uploads',
-        random_file_name + file.filename + '.png'
+        f"{random_file_name}-{file.filename}.png",
     )
     file.save(file_path)
 
-    tesseract_txt = tesseract_read(file_path)
-    keras_txt = keras_read(file_path)
-    easyocr_txt = easyocr_read(file_path)
+    output_file_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        'static',
+        'results',
+        f"{random_file_name}-{file.filename}_result.png",
+    )
+
+    credit_card_data = extract_credit_card_data(
+        file_path,
+        output_file_path
+    )
+
+    validate_credit_card_data(credit_card_data)
+
+    root_dir = os.path.dirname(os.path.realpath(__file__))
+    output_file_path = output_file_path.replace(root_dir, '')
+    current_url = request.url_root
+    output_file_path = current_url + output_file_path
 
     return {
-        'tesseract_txt': tesseract_txt,
-        'keras_txt': keras_txt,
-        'easyocr_txt': easyocr_txt
+        'output_file': output_file_path,
+        'credit_card': credit_card_data,
     }, 200
+
+def extract_credit_card_data(input_path: str, output_path: str):
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(input_path)
+    image = cv2.imread(input_path)
+
+    response = {}
+    for i in range(len(result)):
+        points = np.array(result[i][0], dtype=np.int32)
+        points = points.reshape((-1, 1, 2))
+        cv2.polylines(image, [points], isClosed=True, color=(0, 255, 0), thickness=2)
+        cv2.putText(image, result[i][1], (points[0][0][0], points[0][0][1] - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+        if i == 6:
+            response['number'] = result[i][1]
+
+        if i == 8:
+            response['holder_name'] = result[i][1].upper()
+
+        if i == 10:
+            expiry_date = result[i][1].replace('FIN', '').replace(',', '')
+            expiry_date = expiry_date.replace(':', '')
+            response['expiry_date'] = expiry_date
+
+        if i == 12:
+            response['cvv'] = result[i][1]
+
+    cv2.imwrite(output_path, image)
+
+    return response
+
+def validate_credit_card_data(data: dict):
+    pass
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7000, debug=True)
--- static/script.js
+++ static/script.js
@@ -1,8 +1,81 @@
+const takePicture = async (videoElement) => {
+    const audio = new Audio('/static/Barcode-scanner-beep-sound.mp3');
+    audio.play();
+    showLoader();
+
+    const capturedImageCanvas = document.getElementById('captured-image');
+    const context = capturedImageCanvas.getContext('2d');
+    capturedImageCanvas.width = videoElement.videoWidth;
+    capturedImageCanvas.height = videoElement.videoHeight;
+
+    context.drawImage(
+        videoElement,
+        0,
+        0,
+        videoElement.videoWidth,
+        videoElement.videoHeight
+    );
+
+    const imageData = capturedImageCanvas.toDataURL('image/jpeg');
+    const blob = dataURItoBlob(imageData);
+
+    hideScanArea(imageData);
+
+    const extractedInfo = await extractInfoFromImage(blob);
+
+    displayInformation(extractedInfo);
+
+    hideLoader();
+};
+
+async function extractInfoFromImage(imageData) {
+    const formData = new FormData();
+    formData.append('file', imageData);
+
+    const res = await fetch('/api/upload', {
+        method: 'POST',
+        body: formData,
+    });
+    const { credit_card, output_file } = await res.json();
+
+    return { credit_card, output_file };
+}
+
+function displayInformation({ credit_card, output_file }) {
+    const resultDiv = document.getElementById('result');
+    document.getElementById('cardNumber').value = credit_card.number;
+    document.getElementById('name').value = credit_card.holder_name;
+    document.getElementById('cvv').value = credit_card.cvv;
+    document.getElementById('expirationDate').value = credit_card.expiry_date;
+
+    resultDiv.style.display = 'block';
+    const resultDataDiv = document.getElementById('result-data');
+
+    const src = output_file;
+    const img = document.createElement('img');
+    img.src = src;
+    img.width = 300;
+    img.height = 300;
+
+    const restartBtn = document.createElement('button')
+    restartBtn.className = 'restart-btn';
+    restartBtn.innerHTML = 'Restart';
+    resultDiv.appendChild(restartBtn);
+
+    restartBtn.addEventListener('click', () => {
+        showScanArea();
+        resultDiv.innerHTML = '';
+        resultDiv.style.display = 'none';
+    });
+
+    resultDataDiv.appendChild(img);
+    const capturedImage = document.getElementById('captured-image');
+    capturedImage.style.display = 'none';
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const videoElement = document.getElementById('camera-preview');
     const captureButton = document.getElementById('capture-btn');
-    const capturedImageCanvas = document.getElementById('captured-image');
-    const resultDiv = document.getElementById('result');
 
     navigator.mediaDevices.getUserMedia({ video: true })
         .then((stream) => {
@@ -12,41 +85,30 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error('Error accessing camera:', error);
         });
 
-    captureButton.addEventListener('click', async () => {
-        const context = capturedImageCanvas.getContext('2d');
-        capturedImageCanvas.width = videoElement.videoWidth;
-        capturedImageCanvas.height = videoElement.videoHeight;
-        context.drawImage(videoElement, 0, 0, videoElement.videoWidth, videoElement.videoHeight);
+    captureButton.addEventListener('click', () => takePicture(videoElement));
 
-        const extractedInfo = await extractInfoFromImage(capturedImageCanvas.toDataURL('image/jpeg'));
+    const starBtn = document.getElementById('start-btn');
 
-        displayInformation(extractedInfo);
+    starBtn.addEventListener('click', () => {
+        const timerContainer = document.getElementById('timer');
+        timerContainer.style.display = 'block';
+        let count = 10;
+
+        const timer = setInterval(() => {
+            timerContainer.innerHTML = count;
+            count -= 1;
+            if (count === -1) {
+                clearInterval(timer);
+                timerContainer.style.display = 'none';
+                takePicture(videoElement);
+            }
+        }, 1000);
     });
 
-    async function extractInfoFromImage(imageData) {
-        const blob = dataURItoBlob(imageData);
-        const formData = new FormData();
-        formData.append('file', blob);
-
-        const res = await fetch('http://localhost:7000/api/upload', {
-            method: 'POST',
-            body: formData,
-        });
-        const { image_content } = await res.json();
-
-        return { image_content };
-    }
-
-    function displayInformation(info) {
-        console.log(info);
-        resultDiv.innerHTML = `
-           <p>
-            ${info.image_content}
-           </p>
-        `;
-    }
+    document.addEventListener('keydown', (event) => {
+        takePicture(videoElement);
+    });
 });
-
 
 function dataURItoBlob(dataURI) {
     const splitDataURI = dataURI.split(',');
@@ -59,4 +121,28 @@ function dataURItoBlob(dataURI) {
     }
 
     return new Blob([buffer], { type: contentType, name: 'image.jpeg' });
+}
+
+function hideScanArea(imageData) {
+    const scanArea = document.getElementById('scan-area');
+    scanArea.style.display = 'none';
+
+    const capturedImage = document.getElementById('captured-image');
+    capturedImage.src = imageData;
+    capturedImage.style.display = 'block';
+}
+
+function showScanArea() {
+    const scanArea = document.getElementById('scan-area');
+    scanArea.style.display = 'block';
+}
+
+function showLoader() {
+    const loader = document.getElementById('loader');
+    loader.style.display = 'block';
+}
+
+function hideLoader() {
+    const loader = document.getElementById('loader');
+    loader.style.display = 'none';
 }
